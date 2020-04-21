8e1939723ceaaf12a2336eec7ceecf39b14c8077
170507180119 <2916076379@qq.com>
2020-04-21 14:57:46 +0800

    Initial commit

---
+++ app/src/main/java/com/example/acer/cnedusdwuandroid02homesn170507180119/MainActivity170507180119.java
@@ -0,0 +1,13 @@
+package com.example.acer.cnedusdwuandroid02homesn170507180119;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity170507180119 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main170507180119);
+    }
+}
